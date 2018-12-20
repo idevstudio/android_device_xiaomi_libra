7a040bcb10be06019fd02a08d9dd5f21f3da9ae8
WJXXBSH <zyb_1998@outlook.com>
2018-12-20 22:34:38 +0800

    change dex2oat config

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -77,8 +77,19 @@ WITH_LINEAGE_CHARGER := false
 BOARD_USES_QCNE := true
 
 # Enable dex-preoptimization to speed up first boot sequence
-WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
 WITH_DEXPREOPT := true
+WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := false
+DONT_DEXPREOPT_PREBUILTS := false
+LOCAL_DEX_PREOPT := true
+PRODUCT_DEX_PREOPT_BOOT_FLAGS := --compiler-filter=speed
+PRODUCT_DEX_PREOPT_DEFAULT_FLAGS := --compiler-filter=speed
+PRODUCT_PROPERTY_OVERRIDES += \
+     pm.dexopt.install=speed
+     pm.dexopt.bg-dexopt=speed
+     pm.dexopt.boot=speed
+     pm.dexopt.first-boot=speed
+     dalvik.vm.dex2oat-filter=speed
+     dalvik.vm.image-dex2oat-filter=speed
 
 # Display
 MAX_EGL_CACHE_KEY_SIZE := 12*1024
